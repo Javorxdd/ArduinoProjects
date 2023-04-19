e9265fcaa5191c398b60bd7d4a22401abdfe4cc7
Jiří Javůrek <92877192+Javorxdd@users.noreply.github.com>
2023-04-19 19:15:29 +0200

    Update kytky.ino

--- Project 2 - Flowers/kytky.ino
+++ Project 2 - Flowers/kytky.ino
@@ -12,19 +12,19 @@ void setup() {
 }
 
 void loop() {
-  // nastavení LED diod 1, 3, 5 a 7 na modrou barvu
+  // sets LED 1, 3, 5, 7 on blue color
   strip.setPixelColor(1, 255, 0, 255); // modrá LED 1
   strip.setPixelColor(3, 255, 0, 255); // modrá LED 3
   strip.setPixelColor(5, 255, 0, 255); // modrá LED 5
   strip.setPixelColor(7, 255, 0, 255); // modrá LED 7
 
-  // nastavení LED diod 0, 2, 4 a 6 na červenou barvu
+  // sets LED 0, 2, 4, 6 on red color
   strip.setPixelColor(0, 255, 0, 255); // červená LED 0
   strip.setPixelColor(2, 255, 0, 255); // červená LED 2
   strip.setPixelColor(4, 255, 0, 255); // červená LED 4
   strip.setPixelColor(6, 255, 0, 255); // červená LED 6
 
-  // aktualizace LED modulu s nastavenými barvami a jasem
+  // updates LED to show colors and brightness
   strip.show();
 
 }
